1d278c3c313b976df0c6c2bffba98026b14f95f4
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-07-27 20:33:47 +0545

    Printing Joke Lines using pyjokes module

---
+++ 00.YouTube-Learnings/Code-With-Harry/Chapter-1/module.py
@@ -0,0 +1,35 @@
+# install pyjokes in terminal using: pip install pyjokes
+# pip=> package manager, used to install module in the system
+
+# importing pyjokes
+import pyjokes 
+
+# variable named jokes 
+joke=pyjokes.get_joke()
+# get_joke() is a function which is taken from module get_joke()
+
+# Printing joke lines
+print("Printing Joke Lines.......")
+print(joke)
+
+# Comments are ignored by the compiler, interpreter, computer.
+# Only used/written for understanding the code
+
+# This is single-line Comment. 
+
+"""
+This 
+    is 
+        multi-line
+            comment
+"""
+
+'''
+This 
+    is 
+        also 
+            multi-line
+                comment
+'''
+
+# Multi-line Comments, written either inside """_______""" or '''_______'''
