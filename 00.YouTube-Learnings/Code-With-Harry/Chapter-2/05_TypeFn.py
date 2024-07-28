71fc68bc0016487d2e979b178feeb0a7eab02eba
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-07-28 18:13:25 +0545

    Using type( ) Function in Python

---
+++ 00.YouTube-Learnings/Code-With-Harry/Chapter-2/05_TypeFn.py
@@ -0,0 +1,31 @@
+# Type() Function
+# type() function: is used to find the data type of a given variable in python.. 
+
+# Integer
+a=31
+t=type(a)     #class<int>
+print(t)
+
+# Floating Numbers
+b=31.45
+u=type(b)
+print(u)
+
+# String
+c="Manish"
+c1="21.4"
+c2="21"
+v=type(c)
+v=type(c1)
+v=type(c2)
+print(v)
+
+# Boolean
+d=True
+w=type(d)
+print(w)
+
+# None
+e=None
+x=type(e)
+print(x)
