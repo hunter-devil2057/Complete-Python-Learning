d3be98b955a2e03f9b522febc2ca4d5216ccce8c
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-07-28 17:55:31 +0545

    Variables in Python

---
+++ 00.YouTube-Learnings/Code-With-Harry/Chapter-2/01_variables.py
@@ -0,0 +1,12 @@
+# Variable is a name given to a memory location in a program. 
+a=1     #variable: container to store value
+b=2     #integer (or non-decimal value)
+sum=a+b
+print("Sum: ", sum)
+
+fName="Manish"   #reserved words in Python
+lName="Shiwakoti"   #string are always stored in "___" or '___'
+address='Bhaktapur'
+print(address)
+
+c=31506.22  #identifiers: class/function/variable name
