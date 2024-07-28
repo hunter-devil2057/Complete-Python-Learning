dc5e7a34fae9ee9125d885ff6e791e7a1552f86f
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-07-28 19:12:47 +0545

    Finding Remainder and Quotient after dividing two numbers

---
+++ 00.YouTube-Learnings/Code-With-Harry/Practice-Set/Chapter-2/Problem2.py
@@ -0,0 +1,9 @@
+# Write a Python Program to find out the remainder when a number is dividd by 2
+x=int(input("Enter First Number: "))
+y=int(input("Enter Second Number: "))
+quotient=x/y
+remainder=x%y
+
+print("When ", x ," is divided by ", y)
+print("Quotient=", quotient)
+print("Remainder=", remainder)
