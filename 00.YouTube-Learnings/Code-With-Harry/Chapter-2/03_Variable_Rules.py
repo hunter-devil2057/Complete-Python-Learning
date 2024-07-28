cdb3deafbd1eb83602089b8673f3248081cd0821
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-07-28 17:57:49 +0545

    Rules for Declaring Variables in Python

---
+++ 00.YouTube-Learnings/Code-With-Harry/Chapter-2/03_Variable_Rules.py
@@ -0,0 +1,24 @@
+# Rules for Writing/Defining Variable Name
+    # - A variable name can contain alphabets, digits and underscores. 
+    # - A variable name can only start with an alphabet and underscores
+    # - A variable name can't start with a digit
+    # - No while space is allowed to be used inside a variable name. 
+
+a=23        #valid
+
+aaa=435     #valid 
+
+manish=23   #valid
+
+harish=45   #valid
+
+_manish=34  #valid
+
+# @sameer=467       #Not valid
+# No special characters except (underscore(_)) are used to define variable name
+
+# 1manish="Teacher"       #Not Valid
+# No numeric digit can be used to start variable name
+
+# animal name="Penguin"         #Not Valid
+# No White Space is not allowed
