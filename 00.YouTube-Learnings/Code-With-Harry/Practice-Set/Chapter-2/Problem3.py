5ccec96c6fc518f2cf2f5fe3374b83f8b7f75ae4
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-07-28 19:13:24 +0545

    Checking DataType of a variable assigned by using input( ) function

---
+++ 00.YouTube-Learnings/Code-With-Harry/Practice-Set/Chapter-2/Problem3.py
@@ -0,0 +1,9 @@
+# Check the type of variable assigned by using input() function 
+a=input("Enter the value of a: ")
+print(type(a))
+
+b=int(input("Enter the value of b: "))
+print(type(b))
+
+c=float(input("Enter the value of c: "))
+print(type(c))
