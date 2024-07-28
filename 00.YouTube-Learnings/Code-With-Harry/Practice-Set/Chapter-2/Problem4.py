c259c0c5c2acba3084bc5a0d802353feaf03417e
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-07-28 19:31:04 +0545

    Using Comparison operator to find out whether the given variable a is greater than b or not

---
+++ 00.YouTube-Learnings/Code-With-Harry/Practice-Set/Chapter-2/Problem4.py
@@ -0,0 +1,6 @@
+# Use Comparison operator to find out whether the given variable a is greater than b or not. 
+# Take a=34, b=80
+a=int(input("Enter First Number: "))
+b=int(input("Enter Second Number: "))
+
+print("a is greater than b: ", a>b)
