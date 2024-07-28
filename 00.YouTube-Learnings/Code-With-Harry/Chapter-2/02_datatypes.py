1572841d4df2e02e8bff80dee29bb20052f1ad8d
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-07-28 17:56:16 +0545

    Various Data Types in Python

---
+++ 00.YouTube-Learnings/Code-With-Harry/Chapter-2/02_datatypes.py
@@ -0,0 +1,13 @@
+#Data Types in Python
+# 1. Integers
+# 2. Floating Point Numbers
+# 3. Strings
+# 4. Boolean 
+# 5. None
+
+a=1     #a is an integer
+b=5.22      #b is a floating point number
+c="Harry"   #c is a String
+d=False     # d is a Boolean Variable
+e=None      # e is a none type variable 
+# None is used to denote the no value in the variable 
