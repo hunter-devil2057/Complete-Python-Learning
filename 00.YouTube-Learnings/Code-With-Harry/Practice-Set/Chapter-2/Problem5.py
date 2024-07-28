9b4a7e4edeece76bc7f3d2c3982c3c4c01bf6717
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-07-28 19:31:36 +0545

    Finding out an average of two numbers entered by the user

---
+++ 00.YouTube-Learnings/Code-With-Harry/Practice-Set/Chapter-2/Problem5.py
@@ -0,0 +1,11 @@
+# Write a python program to find out an average of two numbers entered by the user. 
+
+# Receiving two numbers from the user
+a=int(input("Enter First Number: "))
+b=int(input("Enter Second Number: "))
+
+# Operation for finding out average of two numbers
+average=(a+b)/2
+
+# Displaying the result
+print("Average=", average)
