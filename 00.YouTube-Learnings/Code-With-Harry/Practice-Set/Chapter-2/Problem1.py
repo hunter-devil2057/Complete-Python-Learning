83e006245ba21af0d7e0943f0a0f10621a41d93d
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-07-28 19:12:15 +0545

    Adding Two Numbers

---
+++ 00.YouTube-Learnings/Code-With-Harry/Practice-Set/Chapter-2/Problem1.py
@@ -0,0 +1,12 @@
+# Write a python program to add two numbers. 
+# Receiving First Number from the user
+a=float(input("Enter First Number:"))
+
+# Receiving Second Number from the user
+b=float(input("Enter Second Number:"))
+
+# Adding both first and second Number
+sum=a+b
+
+# Displaying the result
+print(a, " + ", b, " = ", sum)
